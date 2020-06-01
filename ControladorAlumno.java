e3b10472f630ebd2110b5801bffae54ec239d2ce
Gemma <56310320+philologic@users.noreply.github.com>
2020-06-02 01:42:05 +0200

    01/6

---
+++ ControladorAlumno.java
@@ -0,0 +1,126 @@
+package proyecto.controlador;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import proyecto.modelo.Alumno;
+import proyecto.modelo.Entrenamiento;
+import proyecto.modelo.TipoEjercicio;
+import proyecto.principal.MenuEjecucion;
+
+public class ControladorAlumno extends ControladorUsuario{
+    
+    /**
+     * Solicita datos para crear un nuevo alumno.
+     * @throws SQLException 
+     */
+    public static void crearNuevoAlumno() throws SQLException{
+        Alumno alu = new Alumno();
+        System.out.println("Introduce los siguientes datos del nuevo alumno:");
+        crearNuevoUsuario(alu);
+        String userIban = "";
+        do {
+            System.out.println("  -IBAN para domiciliacion:");
+            userIban = MenuEjecucion.lector.nextLine().toUpperCase().trim();
+        } while (!validarIban(userIban));
+        alu.setIban(userIban);
+        introducirNuevoAlumno(alu);
+        System.out.println("¿Deseas establecer un tipo de entrenamiento solicitado para " + alu.getNombre()+ "?");
+        System.out.println("  1- sí");
+        System.out.println("  2- no");
+        String opcionUserPrograma = MenuEjecucion.lector.nextLine().trim();
+        if (opcionUserPrograma.equals("1")) {
+            ControladorEntrenamiento.solicitarEntrenamiento(alu.getDni());
+        }
+        System.out.println("Alumno creado con éxito.");
+    }
+    
+    /**
+     * Crea un objeto tipo Alumno desde valores obtenidos en tablas.<br> Se
+     * utiliza para varias funciones.
+     *
+     * @param id DNI del alumno
+     * @return objeto tipo Alumno
+     * @throws SQLException
+     */
+    public static Alumno generarAlumnoDesdeTabla(String id) throws SQLException {
+        Alumno user = new Alumno();
+        String queryUsuario = "SELECT * FROM usuario WHERE DNI = ?;";
+        PreparedStatement prepStat = MenuEjecucion.con.prepareStatement(queryUsuario);
+        prepStat.setString(1, id);
+        ResultSet results = prepStat.executeQuery();
+        results.next();
+        user.setDni(id);
+        user.setNombre(results.getString("nombre"));
+        user.setPassword(results.getString("password"));
+        user.setApellido1(results.getString("apellido1"));
+        user.setApellido2(results.getString("apellido2"));
+        user.setEmail(results.getString("email"));
+        user.setTelefono(results.getInt("telefono"));
+        user.setDireccion(results.getString("direccion"));
+        String tipoDeEjercicio = results.getString("tipo_prog_solicitado");
+        if (tipoDeEjercicio != null) {
+            user.setTipoEjercicio(TipoEjercicio.valueOf(tipoDeEjercicio));
+        }
+        results.close();
+        prepStat.close();
+        return user;
+    }
+
+    //Al ser un insert, por transacción. Sólo hago sin transacción las meras consultas.
+    /**
+     * Pasa un objeto Alumno a la tabla
+     * @throws SQLException 
+     */
+    public static void introducirNuevoAlumno(Alumno alu) throws SQLException {
+        boolean estadoAC = MenuEjecucion.con.getAutoCommit();
+        try {
+            MenuEjecucion.con.setAutoCommit(false);
+            String query = "INSERT INTO usuario (DNI, password, discriminador, nombre, apellido1, apellido2, email, telefono, direccion, IBAN) "
+                    + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+            PreparedStatement prepStat = MenuEjecucion.con.prepareStatement(query);
+            prepStat.setString(1, alu.getDni());
+            prepStat.setString(2, alu.getPassword());
+            prepStat.setString(3, "alumno");
+            prepStat.setString(4, alu.getNombre());
+            prepStat.setString(5, alu.getApellido1());
+            prepStat.setString(6, alu.getApellido2());
+            prepStat.setString(7, alu.getEmail());
+            prepStat.setInt(8, alu.getTelefono());
+            prepStat.setString(9, alu.getDireccion());
+            prepStat.setString(10, alu.getIban());
+            prepStat.execute();
+            MenuEjecucion.con.commit();
+            System.out.println("Nuevo alumno (" + alu.getNombre() + " " + alu.getApellido1() + ") introducido correctamente.");
+            prepStat.close();
+        } catch (SQLException sqle) {
+            sqle.printStackTrace();
+            System.out.println("Error en la introducción del alumno.");
+            MenuEjecucion.con.rollback();
+        } finally {
+            MenuEjecucion.con.setAutoCommit(estadoAC);
+        }
+    }
+
+    /**
+     * Crea un objeto de tipo alumno a partir de su dni (tomando datos de la BD)
+     *
+     * @param dni identificador del usuario tipo alumno
+     * @return Alumno objeto tipo alumno
+     */
+    public static Alumno crearObjetoAlumno(String dni) throws SQLException {
+        Alumno alumno = new Alumno();
+        ResultSet lineaAlumno = buscarUsuarioPorDni(dni);
+        lineaAlumno.next();
+        alumno.setDni(dni);
+        alumno.setNombre(lineaAlumno.getString("nombre"));
+        alumno.setApellido1(lineaAlumno.getString("apellido1"));
+        alumno.setApellido2(lineaAlumno.getString("apellido2"));
+        alumno.setEmail(lineaAlumno.getString("email"));
+        alumno.setTelefono(lineaAlumno.getInt("telefono"));
+        alumno.setDireccion(lineaAlumno.getString("direccion"));
+        alumno.setIban("IBAN");
+        lineaAlumno.close();
+        return alumno;
+    }
+}
---
+++ ControladorUsuario.java
@@ -0,0 +1,179 @@
+package proyecto.controlador;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import proyecto.principal.MenuEjecucion;
+import proyecto.modelo.*;
+
+/**
+ * Incluye los métodos que trabajan con objetos y procesos relacionados con la clase modelo.Usuario
+ */
+public class ControladorUsuario {
+//Métodos:
+    /**
+     * Permite encontrar un usuario en base a su DNI.
+     *
+     * @param dni
+     * @return queryResult (tipoResultSet)
+     * @throws SQLException
+     */
+    public static ResultSet buscarUsuarioPorDni(String dni) throws SQLException {
+        String query = "SELECT * FROM usuario WHERE DNI = ?;";
+        PreparedStatement prepStat = MenuEjecucion.con.prepareStatement(query);
+        prepStat.setString(1, dni);
+        ResultSet queryResult = prepStat.executeQuery();
+        //Nota: queryResult no se debe cerrar aquí sino en el método que lo llama
+        prepStat.close();
+        return queryResult;
+    }
+
+    /**
+     * Comprueba si el valor que se pasa existe en tablas.
+     *
+     * @param texto cuya validez y/o existencia en tablas se desea comprobar
+     * @return verdadero si está en tablas
+     */
+    public static String comprobarUsuario(String texto) throws SQLException {
+        String password = "error";
+        String query = "SELECT password FROM USUARIO WHERE DNI = ?;";
+        PreparedStatement prepStat = MenuEjecucion.con.prepareStatement(query);
+        prepStat.setString(1, texto);
+        ResultSet queryResult = prepStat.executeQuery();
+        //Nota: Como no tenemos método isEmpty, la solución es:
+        if (queryResult.next() != false) {
+            password = queryResult.getString("password");
+        }
+        //lógica de esto: si existe un resultado de esta búsqueda, entonces existe la entrada: es válida
+        queryResult.close();
+        prepStat.close();
+        //aprovecho para obtener la contraseña y así no tengo que hacer la búsqueda mil veces
+        return password;
+    }
+    
+    /**
+     * Comprueba qué tipo de usuario es, y devuelve true si es entrenador.
+     *
+     * @param id identificador de usuario
+     * @return true si es entrenador, false si es alumno
+     */
+    public static boolean comprobarTipoUsuario(String id) throws SQLException {
+        String query = "SELECT discriminador FROM USUARIO WHERE DNI = ?;";
+        PreparedStatement prepStat = MenuEjecucion.con.prepareStatement(query);
+        prepStat.setString(1, id);
+        ResultSet queryResult = prepStat.executeQuery();
+        if (queryResult.next()) {               //Comprobando que no me lo devuelva vacío.
+            if (queryResult.getString("discriminador").equals("entrenador")) {
+                return true;
+            }
+        }
+        queryResult.close();
+        prepStat.close();
+        return false;
+    }
+    
+    /**
+     * Pide los datos del nuevo usuario por consola y los setea en el objeto que devuelve.
+     */
+    public static void crearNuevoUsuario(Usuario user) {
+        String userDni = "";
+        do {
+            System.out.println("  -dni:");
+            userDni = MenuEjecucion.lector.nextLine().toUpperCase().trim();
+        } while (!validarDni(userDni));
+        user.setDni(userDni);
+        System.out.println("  -contraseña:");
+        String userPw = MenuEjecucion.lector.nextLine().toLowerCase().trim();
+        user.setPassword(userPw);
+        //TODO: confirmarContraseña()
+        System.out.println("  -nombre:");
+        String userNombre = MenuEjecucion.lector.nextLine();
+        user.setNombre(userNombre);
+        System.out.println("  -primer apellido:");
+        String userApellido1 = MenuEjecucion.lector.nextLine();
+        user.setApellido1(userApellido1);
+        System.out.println("  -segundo apellido:");
+        String userApellido2 = MenuEjecucion.lector.nextLine();
+        user.setApellido2(userApellido2);
+        System.out.println("  -e-mail:");
+        String userEmail = MenuEjecucion.lector.nextLine();
+        user.setEmail(userEmail);
+        System.out.println("  -telefono:");
+        int userPhone = Integer.parseInt(MenuEjecucion.lector.nextLine());
+        user.setTelefono(userPhone);
+        System.out.println("  -direccion:");
+        String userAdress = MenuEjecucion.lector.nextLine();
+        user.setDireccion(userAdress);
+    }
+
+    /**
+     * Es una validación básica, no buscamos que la letra coincida porque los datos preintroducidos son inventados.<br>
+     * Dos comprobaciones: que los ocho primeros caracteres sean números (ascii entre 48 y 57) y que el último sea letra.
+     * Sólo consideramos DNI, no NIE ni pasaportes.
+     * @param dni
+     * @return true si cumple los requisitos de validez
+     */
+    public static boolean validarDni(String dni){
+        boolean dniValido = true;
+        if (dni.length() != 9 || !Character.isLetter(dni.charAt(8))) {
+            dniValido = false;
+        } else {
+            for (int i = 0; i < dni.length() - 1; i++) {
+                int numAscii = dni.codePointAt(i);
+                boolean numValido = (numAscii > 47 && numAscii < 58);
+                if (numValido == false) {
+                    dniValido = false;
+                }
+            }
+        }
+        if (dniValido == false) {
+            System.out.println("Este DNI no cumple los requisitos.");
+        }
+        return dniValido;
+    }
+    public static boolean validarIban (String iban){
+        boolean ibanvalido = true;
+        if (iban.length() != 24 || Character.isLetter(iban.charAt(2))) {
+            ibanvalido = false;
+        } else {
+            for (int i = 2; i < iban.length() ; i++) {
+                int numAscii = iban.codePointAt(i);
+                boolean numValido = (numAscii > 47 && numAscii < 58);
+                if (numValido == false) {
+                    ibanvalido = false;
+                }
+            }
+        }
+        if (ibanvalido == false) {
+            System.out.println("Este IBAN no cumple los requisitos.");
+        }
+        return ibanvalido;
+    }
+    
+    /**
+     * Obtiene el tipo de programa que ha solicitado un alumno concreto
+     * 
+     * @param idAlumno identificador del alumno
+     * @return tipo de programa solicitado (objeto TipoEjercicio)
+     * @throws SQLException 
+     */ 
+    public static TipoEjercicio obtenerSolicitudUsuario(String idAlumno) throws SQLException {
+        String query = "SELECT tipo_prog_solicitado FROM usuario WHERE DNI = ?;";      //sé que el último que he creado es el mayor, por serial
+        PreparedStatement prepStat = MenuEjecucion.con.prepareStatement(query);
+        prepStat.setString(1, idAlumno);
+        ResultSet results = prepStat.executeQuery();
+        results.next();
+        String tipoString = results.getString(1);
+        results.close();
+        prepStat.close();
+        if (tipoString != null) {
+            return TipoEjercicio.valueOf(tipoString);
+        }
+        return null;
+    }
+    
+    public static boolean validarPassword(String pw){
+        //TODO
+        return false;
+    }
+}
