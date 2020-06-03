d81ad38c3f620776a20b58f5fe0a17f6828212fa
Gemma <56310320+philologic@users.noreply.github.com>
2020-06-03 22:49:22 +0200

    06/03

--- ProyectoFinal/src/proyecto/controlador/Utilidades.java
+++ ProyectoFinal/src/proyecto/controlador/Utilidades.java
@@ -10,23 +10,25 @@ import java.util.Date;
  * Incluye métodos con utilidades genéricas.
  */
 public class Utilidades {
-    
+
     /**
      * Toma la fecha actual y la adapta al formato de la base de datos.
+     *
      * @return tipo String con la fecha en formato yyyy-MM-dd
      */
     public static String obtenerFecha() {
         return new SimpleDateFormat("yyyy-MM-dd").format(new Date());
     }
-    
+
     /**
      * Toma la fecha actual y la adapta al formato del archivo de errores.
+     *
      * @return tipo String con la fecha en formato yyyy-MM-dd
      */
     public static String obtenerFechaHora() {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     }
-    
+
     /**
      * Guarda los errores encontrados en un archivo en el proyecto.
      */
@@ -41,4 +43,25 @@ public class Utilidades {
         }
     }
     
+    /**
+     * 
+     * 
+     * @param str
+     * @return string sin espacios y con la primera letra en mayúcula
+     */
+
+    public static String modificarString(String str) {
+        String strsinespacios = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != ' ') {
+                strsinespacios += str.charAt(i);
+            }
+        }
+        if (strsinespacios == null || strsinespacios.isEmpty()) {
+            return strsinespacios;
+        } else {
+            return strsinespacios.substring(0, 1).toUpperCase() + strsinespacios.substring(1);
+        }
+    }
+
 }
